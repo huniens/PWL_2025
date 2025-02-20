123b7b2ef79a11341b59c98909ec597285445294
huniens <huniens06@gmail.com>
2025-02-20 13:26:22 +0700

    JS2 - Route that displays a 'Welcome' message

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/world', function ()
-    { return 'World';
+Route::get('/Welcome', function () { return 'Welcome Hanin Mariam Abiyyah Hendrik NIM 2341760154';
 });
+
+
 
