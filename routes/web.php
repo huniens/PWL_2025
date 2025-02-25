16c4ed5d32291c21ea7b223e99a38126834196af
huniens <huniens06@gmail.com>
2025-02-25 20:17:45 +0700

    JOBSHEET 2 - Resource Controller

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,13 @@
 <?php
 
-use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
-use app\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PhotoController;
+
+Route::resource('photos', PhotoController::class)->except([
+    'destroy'
+]);
 
 
-Route::get('/', [PageController::class, 'index'])->name('home');
-
-Route::get('/about', [PageController::class, 'about'])->name('about');
-
-Route::get('/articles/{id}', [PageController::class, 'articles'])->name('articles');
 
 
 
